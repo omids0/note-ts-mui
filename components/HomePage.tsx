70d2277a86eaf4cb0828234068385269da70f5f7
omids0 <soheilniao@gmail.com>
2022-06-22 09:08:24 +0430

    home page added

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,8 +1,19 @@
-import { Stack } from '@mui/material'
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import React from "react";
 
 export default function HomePage() {
+  const style = {
+    width: "100%",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  
   return (
-    <Stack>HomePage</Stack>
-  )
+    <Stack sx={style}>
+      <Box>test</Box>
+    </Stack>
+  );
 }
