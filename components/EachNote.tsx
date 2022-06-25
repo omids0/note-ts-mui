e85a246b7c4d5927788629282f79413313384829
omids0 <soheilniao@gmail.com>
2022-06-25 09:38:55 +0430

    animation added for each note

--- components/EachNote.tsx
+++ components/EachNote.tsx
@@ -1,5 +1,13 @@
-import { Box, Button, Card, Modal, Stack, TextField } from "@mui/material";
-import React, { FC, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Grow,
+  Modal,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { FC, useEffect, useState } from "react";
 import { INoteDataReq } from "../types/DTO/req";
 import { INoteDataRes } from "../types/DTO/res";
 import { useDispatch } from "react-redux";
@@ -26,6 +34,11 @@ const EachNote: FC<INoteItemProps> = (props) => {
   });
 
   const [open, setOpen] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+  }, []);
 
   const style = {
     position: "absolute" as "absolute",
@@ -50,8 +63,8 @@ const EachNote: FC<INoteItemProps> = (props) => {
   };
 
   const handlerUpdate = () => {
-    console.log('update',state);
-    dispatch(updateNoteActionRequest(state, setOpen))
+    console.log("update", state);
+    dispatch(updateNoteActionRequest(state, setOpen));
   };
 
   const handlerRemove = () => {
@@ -60,56 +73,62 @@ const EachNote: FC<INoteItemProps> = (props) => {
   };
 
   return (
-    <Card sx={{ margin: "1rem" }}>
-      <Stack sx={{ padding: "1rem", bgcolor: "#eeeeee" }}>{title}</Stack>
-      <Stack sx={{ padding: "1rem" }} onClick={() => setOpen(!open)}>
-        {description}
-      </Stack>
-      <Modal
-        open={open}
-        onClose={() => setOpen(!open)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <TextField
-            id="filled-basic"
-            label="Title"
-            variant="filled"
-            value={state.title}
-            name="title"
-            onChange={handlerInput}
-            sx={{ backgroundColor: "#ffffff", margin: "1rem 0" }}
-          />
-          <TextField
-            id="filled-basic"
-            multiline
-            label="Description"
-            variant="filled"
-            value={state.description}
-            name="description"
-            onChange={handlerInput}
-            sx={{ backgroundColor: "#ffffff" }}
-          />
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <Button
-              variant="contained"
-              sx={{ margin: "3rem 0 0", bgcolor: "green" }}
-              onClick={handlerUpdate}
-            >
-              Update Note
-            </Button>
-            <Button
-              variant="contained"
-              sx={{ margin: "3rem 0 0", bgcolor: "red" }}
-              onClick={handlerRemove}
-            >
-              Remove Note
-            </Button>
+    <Grow
+      in={checked}
+      style={{ transformOrigin: "0 0 0" }}
+      {...(checked ? { timeout: 500 } : {})}
+    >
+      <Card sx={{ margin: "1rem" }}>
+        <Stack sx={{ padding: "1rem", bgcolor: "#eeeeee" }}>{title}</Stack>
+        <Stack sx={{ padding: "1rem" }} onClick={() => setOpen(!open)}>
+          {description}
+        </Stack>
+        <Modal
+          open={open}
+          onClose={() => setOpen(!open)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <TextField
+              id="filled-basic"
+              label="Title"
+              variant="filled"
+              value={state.title}
+              name="title"
+              onChange={handlerInput}
+              sx={{ backgroundColor: "#ffffff", margin: "1rem 0" }}
+            />
+            <TextField
+              id="filled-basic"
+              multiline
+              label="Description"
+              variant="filled"
+              value={state.description}
+              name="description"
+              onChange={handlerInput}
+              sx={{ backgroundColor: "#ffffff" }}
+            />
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Button
+                variant="contained"
+                sx={{ margin: "3rem 0 0", bgcolor: "green" }}
+                onClick={handlerUpdate}
+              >
+                Update Note
+              </Button>
+              <Button
+                variant="contained"
+                sx={{ margin: "3rem 0 0", bgcolor: "red" }}
+                onClick={handlerRemove}
+              >
+                Remove Note
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Modal>
-    </Card>
+        </Modal>
+      </Card>
+    </Grow>
   );
 };
 
